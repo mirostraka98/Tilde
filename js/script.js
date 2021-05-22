7ae4f7a45ee3312397e8ed580031ad2939f851f9
mirostraka98 <84269891+mirostraka98@users.noreply.github.com>
2021-05-23 01:21:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var cisloJedna = +prompt("Napíš prvé číslo");
+var cisloDva = +prompt("Napíš druhé číslo");
+
+alert (cisloJedna + cisloDva)
+
+if((cisloJedna + cisloDva) % 2 == 0) {
+    alert ("Číslo je párne")
+}
+
+else {
+    alert ("Číslo je nepárne")
+}
